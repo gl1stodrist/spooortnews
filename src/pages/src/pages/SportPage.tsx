675d2d4304b0397f91513870326458c0dabdd023
gl1stodrist <ivnovbogdan@gmail.com>
2026-02-17 00:45:31 +0500

    Create SportPage.tsx

---
+++ src/pages/src/pages/SportPage.tsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom'
+
+export default function SportPage() {
+  const { sport } = useParams()
+  return (
+    <div className="min-h-screen pt-24 bg-black text-white flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-black text-yellow-400">
+        {sport ? sport.toUpperCase() : 'Категория'}
+      </h1>
+      <p className="text-2xl text-gray-400 mt-8">
+        Прогнозы по этой категории скоро появятся!
+      </p>
+    </div>
+  )
+}
